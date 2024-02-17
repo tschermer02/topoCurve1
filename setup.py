0fa41c17cad4871ba916f65e2c1763dc3523672d
Taylor Schermer <tschermer02@gmail.com>
2024-02-17 13:00:14 -0700

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='intrinsic and extrinsic curvatures of topographic surfaces',
+    author='Nate Klema, Taylor Schermer, Joel Nash',
+    license='',
+)
