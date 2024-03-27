5b76713a6bf900654a43364cb105dde616b40a3d
Taylor Schermer <tschermer02@gmail.com>
2024-03-27 09:52:39 -0600

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 setup(
-    name='src',
-    packages=find_packages(),
+    name='topo_curve',
     version='0.1.0',
-    description='intrinsic and extrinsic curvatures of topographic surfaces',
-    author='Nate Klema, Taylor Schermer, Joel Nash',
-    license='',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'Pillow',
+        'photutils',
+        'tifffile',
+        'geotiff'
+    ],
+    python_requires='>=3.6',
+    author='Taylor Schermer, Joel Nash, Nate Klema',
+    author_email='tschermer@fortlewis.edu, jxnash@fortlewis.edu, ntklema@fortlewis.edu',
+    description='A package for processing digital elevation models.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/topo_curve',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
 )
