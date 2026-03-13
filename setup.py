bfb91eb6b735e44299ac840380729ca22f8eaca1
Taylor Schermer <tschermer02@gmail.com>
2026-03-13 09:30:44 -0600

    version 0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='topocurve',
-    version='0.1.6',
+    version='0.1.7',
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
